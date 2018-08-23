2541c6d85fd8b0478013830065a45464c81aa232
Teerasej <teerasej@gmail.com>
2018-08-23 12:37:51 +0700

    Create second page for navigated to

---
+++ lib/DetailPage.dart
@@ -0,0 +1,18 @@
+
+
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Detail"),
+      ),
+      body: Center(
+        child: Text("หน้า 2"),
+      ),
+    );
+  }
+
+}
