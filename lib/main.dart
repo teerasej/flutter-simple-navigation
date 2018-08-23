4a4b5b7d303c7ea87d019937515252f4adbdb198
Teerasej <teerasej@gmail.com>
2018-08-23 12:37:36 +0700

    Init with Material app

---
+++ lib/main.dart
@@ -0,0 +1,11 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:navigation_example/HomePage.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: "Nextflow Nav",
+    home: HomePage(),
+  ));
+}
